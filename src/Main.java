3b2145ea36664726295f141d3b41e183d0045a60
hyeryeonkim <kim5638yw@naver.com>
2020-06-15 19:51:28 +0900

    이게 뭐여...

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-//아니 이게 뭐냐고오오오옹 왜 안되냐고오 !!!! 
+//이제 되는건가???????? 야아 !!!!!  
 public class Main {
 
 	public static void main(String[] args) {
 		String str = "나는 할 수 있다.";
 		System.out.println(str);
-
+		String str2 = "뭐니????";
 	}
-
+	
 }
